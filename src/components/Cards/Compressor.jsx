54967b13f391176d261d1d690b577838b0047eb7
Pranav <responsetopranav@gmail.com>
2024-07-01 22:23:11 +0530

    Final commit

--- src/components/Cards/Compressor.jsx
+++ src/components/Cards/Compressor.jsx
@@ -2,36 +2,36 @@ import React from 'react';
 import CardComponent from '../CardComponent';
 import { Box, Grid } from '@mui/material';
 
-const Compressor= () => {
+const Compressor = () => {
     const cardData = [
         {
             imageUrl: "https://i.postimg.cc/pLyQNHLf/image.png",
             title: "OIL COMPRESSOR",
             price: "₹7094 ",
             details: {
-                "model":"XTOC 76",
+                "model": "XTOC 76",
                 "volume": "10L",
                 "input_power": "1HP",
                 "rated_voltage": "220V",
                 "air_exhaust": "110L/min",
                 "pressure": "8 bar",
                 "speed": "2850 r/min"
-}
+            }
 
         },
-     
+
         {
             imageUrl: "https://i.postimg.cc/zXvs71tm/image.png",
             title: "OIL COMPRESSOR",
             price: "₹7200",
             details: {
-                Model:"XTOC 77",
-                 "VOLUME": "30 L",
-  "INPUT POWER": "2.5 HP",
-  "RATED VOLTAGE": "220V",
-  "AIR EXHAUST": "110L/MIN",
-  "PRESSURE": "8 BAR",
-  "SPEED": "2850 r/min"
+                Model: "XTOC 77",
+                "VOLUME": "30 L",
+                "INPUT POWER": "2.5 HP",
+                "RATED VOLTAGE": "220V",
+                "AIR EXHAUST": "110L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2850 r/min"
             }
         },
         {
@@ -39,14 +39,14 @@ const Compressor= () => {
             title: "OIL COMPRESSOR (30L) XLNT",
             price: "₹7335",
             details: {
-                  Model:"XTOC 78",
-                  "VOLUME": "30 L",
-                  "INPUT POWER": "",
-                  "RATED VOLTAGE": "",
-                  "AIR EXHAUST": "",
-                  "PRESSURE": "",
-                  "SPEED": ""
-                
+                Model: "XTOC 78",
+                "VOLUME": "30 L",
+                "INPUT POWER": "",
+                "RATED VOLTAGE": "",
+                "AIR EXHAUST": "",
+                "PRESSURE": "",
+                "SPEED": ""
+
             }
         },
         {
@@ -54,14 +54,14 @@ const Compressor= () => {
             title: "OIL COMPRESSOR (50L) XLNT",
             price: "₹8467",
             details: {
-            Model:"XTOC 79",
-            "VOLUME": "50 L",
-            "INPUT POWER": "",
-            "RATED VOLTAGE": "",
-            "AIR EXHAUST": "",
-            "PRESSURE": "",
-            "SPEED": ""
-          
+                Model: "XTOC 79",
+                "VOLUME": "50 L",
+                "INPUT POWER": "",
+                "RATED VOLTAGE": "",
+                "AIR EXHAUST": "",
+                "PRESSURE": "",
+                "SPEED": ""
+
             }
         },
         {
@@ -69,14 +69,14 @@ const Compressor= () => {
             title: "OIL COMPRESSOR -MSI",
             price: "₹7094",
             details: {
-                Model:"MSOC 6",
+                Model: "MSOC 6",
                 "VOLUME": "10 L",
                 "INPUT POWER": "1 HP",
                 "RATED VOLTAGE": "220V",
                 "AIR EXHAUST": "110L/MIN",
                 "PRESSURE": "8 BAR",
                 "SPEED": "2850 r/min"
-            
+
             }
         },
         {
@@ -84,7 +84,7 @@ const Compressor= () => {
             title: "OIL COMPRESSOR -MSI",
             price: "₹7335",
             details: {
-                Model:"MSOC 7",
+                Model: "MSOC 7",
                 "VOLUME": "30 L",
                 "INPUT POWER": "2.5 HP",
                 "RATED VOLTAGE": "380V",
@@ -93,16 +93,298 @@ const Compressor= () => {
                 "SPEED": "2800 r/min"
             }
         },
-        // {
-        //     imageUrl: "https://i.postimg.cc/MZR90Wrh/image.png",
-        //     title: "ELONG WASHER STRAIGHT (Pack of 50)",
-        //     price: "₹1,000.00 + 18% Tax",
-        //     details: {
-            
-        //     }
-        // },
+        {
+            imageUrl: "https://i.postimg.cc/4dQxZTV4/image.png",
+            title: "OIL COMPRESSOR-MSI",
+            price: "₹8467",
+            details: {
+                Model: "MSOC 8",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "3 HP",
+                "RATED_VOLTAGE": "380V",
+                "AIR_EXHAUST": "233L/MIN",
+                "PRESSURE": "8 BAR Psi",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/9QHbgLR2/image.png",
+            title: "OIL COMPRESSOR",
+            price: "₹24670",
+            details: {
+                Model: "MSOC 9",
+                "VOLUME": "100 L",
+                "INPUT_POWER": "1.5 KW",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "170L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2850 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/9QHbgLR2/image.png",
+            title: "OIL COMPRESSOR",
+            price: "₹51765",
+            details: {
+                Model: "MSOC 10",
+                "VOLUME": "200 L",
+                "INPUT_POWER": "7.5 KW",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "250L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "1450 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/HksnkGj3/image.png",
+            title: "OIL COMPRESSOR",
+            price: "₹42665",
+            details: {
+                Model: "MSOC 11",
+                "VOLUME": "300 L",
+                "INPUT_POWER": "4 KW",
+                "RATED_VOLTAGE": "380V",
+                "AIR_EXHAUST": "600L/MIN",
+                "PRESSURE": "12.5 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/9QHbgLR2/image.png",
+            title: "OIL COMPRESSOR",
+            price: "₹58935",
+            details: {
+                Model: "MSOC 12",
+                "VOLUME": "300 L",
+                "INPUT_POWER": "7.5 KW",
+                "RATED_VOLTAGE": "415V",
+                "AIR_EXHAUST": "900L/MIN",
+                "PRESSURE": "12.5 bar",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/XJFdkMfd/image.png",
+            title: "OIL COMPRESSOR",
+            price: "₹8267",
+            details: {
+                Model: "TH300030",
+                "VOLUME": "30 L",
+                "INPUT_POWER": "2.5 HP",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "169L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2850 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/y81qqsW6/image.png",
+            title: "OIL COMPRESSOR TR-22-50 NEW",
+            price: "₹9615",
+            details: {
+                Model: "TH500050",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "2.5 HP",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "169L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/X7yKVCGQ/image.png",
+            title: "OIL COMPRESSOR ",
+            price: "₹8135",
+            details: {
+                Model: "DOC 1",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "2.5 HP",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "130L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/W38Vqzdy/image.png",
+            title: "OIL FREE COMPRESSOR ",
+            price: "₹11600",
+            details: {
+                Model: "XLAC-224",
+                "VOLUME": "3 L",
+                "INPUT_POWER": "1700 W / 2.27 HP",
+                "RATED_VOLTAGE": "220V",
+                "AIR_DELIVERY": "150L/MIN",
+                "PRESSURE": "0.3-0.8 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/vB0BF3tD/image.png",
+            title: "OIL FREE COMPRESSOR ",
+            price: "₹7200",
+            details: {
+                Model: "XTOFC 79",
+                "VOLUME": "10 L",
+                "INPUT_POWER": "1 HP",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "40L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "1450 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/vB0BF3tD/image.png",
+            title: "OIL FREE COMPRESSOR ",
+            price: "₹7735",
+            details: {
+                Model: "XTOFC 80",
+                "VOLUME": "30 L",
+                "INPUT_POWER": "550 W",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "60L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "1450 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/vB0BF3tD/image.png",
+            title: "OIL FREE COMPRESSOR-550W ",
+            price: "₹9100",
+            details: {
+                Model: "XTOFC 81",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "550 W",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "70L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "1450 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/1XQDz9g4/image.png",
+            title: "OIL FREE COMPRESSOR-750W ",
+            price: "₹9100",
+            details: {
+                Model: "XTOFC 82",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "750 W",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "70L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "1450 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/GmtBjBGv/image.png",
+            title: "OIL FREE COMPRESSOR-550W*2",
+            price: "₹13035",
+            details: {
+                Model: "XTOFC 83",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "550W * 2",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "120L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "1450 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/j54NFp5f/image.png",
+            title: "OIL FREE COMPRESSOR XL -1600-30",
+            price: "₹7800",
+            details: {
+                Model: "XTOFC 84",
+                "VOLUME": "30 L",
+                "INPUT_POWER": "1680 W",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "167L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/xTfC5kMt/image.png",
+            title: "OIL FREE COMPRESSOR XL-1600-50",
+            price: "₹9470",
+            details: {
+                Model: "XTOFC 85",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "1680 W",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "167L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/7Ps3gyTG/image.png",
+            title: "OIL FREE COMPRESSOR (XL-3200-50)2 HEAD",
+            price: "₹14335",
+            details: {
+                Model: "XTOFC 86",
+                "VOLUME": "50 L",
+                "INPUT_POWER": "1680 W*2",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "334L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/0NyD48Fq/image.png",
+            title: "OIL FREE COMPRESSOR -OFC 80L",
+            price: "₹19270",
+            details: {
+                Model: "XTOFC 87",
+                "VOLUME": "80 L",
+                "INPUT_POWER": "2890 W*2",
+                "RATED_VOLTAGE": "220V",
+                "AIR_EXHAUST": "334L/MIN",
+                "PRESSURE": "8 BAR",
+                "SPEED": "2800 r/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/3xJztG8r/image.png",
+            title: "OIL FREE COMPRESSOR -OFC 100L",
+            price: "₹29135",
+            details: {
+                Model: "XTOFC 88",
+                "VOLUME": "100 L",
+                "POWER": "4671W*3",
+                "RATED_VOLTAGE": "220V",
+                "CURRENT": "21.52A/3",
+                "WEIGHT": "52KG"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/3xJztG8r/image.png",
+            title: "OIL FREE COMPRESSOR -OFC 120L",
+            price: "₹32870",
+            details: {
+                Model: "XTOFC 89",
+                "VOLUME": "120 L",
+                "POWER": "4671W*3",
+                "RATED_VOLTAGE": "220V",
+                "CURRENT": "21.52A/3",
+               
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/V68Nr4wL/image.png",
+            title: "OIL FREE COMPRESSOR -OFC 180L",
+            price: "₹42600",
+            details: {
+                Model: "XTOFC 90",
+                "VOLUME": "180 L",
+                "POWER": "6040 W*4",
+                "RATED_VOLTAGE": "400V",
+                "CURRENT": "12.98A/4",
+               
+            }
+        },
     ];
-
     return (
         <Box sx={{ flexGrow: 1, padding: '20px', marginLeft: '50px' }}>
             <Grid container spacing={2}>
--- src/components/Cards/VaccumCleaner.jsx
+++ src/components/Cards/VaccumCleaner.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 import CardComponent from '../CardComponent';
 import { Box, Grid } from '@mui/material';
 
-const VaccumCleaner= () => {
+const VaccumCleaner = () => {
     const cardData = [
         {
             imageUrl: "https://i.postimg.cc/prwW3xTL/image.png",
             title: "VACCUM CLEANER XL-15L",
             price: "₹2300",
             details: {
-                "model":"XTVC 39",
+                "model": "XTVC 39",
                 "power": "1200W",
                 "rated_voltage": "220V",
                 "motor": "single",
                 "tank": "15L",
                 "wind_volume": "2.6m3/min"
-}
+            }
         },
-     
+
         {
             imageUrl: "https://i.postimg.cc/Cxh87TZV/image.png",
             title: "VACCUM CLEANER 6601-20",
             price: "₹3400",
             details: {
-                "model":"XTVC 40",
+                "model": "XTVC 40",
                 "power": "1200W",
                 "rated_voltage": "220V",
                 "motor": "single",
                 "tank": "20L",
                 "wind_volume": "2.6m3/min"
-            
+
             }
         },
         {
@@ -37,12 +37,12 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER 603-20-A ",
             price: "₹3935",
             details: {
-                "model":"XTVC 41","power": "1200W",
-                    "rated_voltage": "230V",
-                    "motor": "single",
-                    "tank": "35L",
-                    "wind_volume": "2.6m3/min"
-            
+                "model": "XTVC 41", "power": "1200W",
+                "rated_voltage": "230V",
+                "motor": "single",
+                "tank": "35L",
+                "wind_volume": "2.6m3/min"
+
             }
         },
         {
@@ -50,7 +50,7 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER",
             price: "₹7470",
             details: {
-                model:" WDUC-25L",
+                model: " WDUC-25L",
                 "power": "",
                 "rated_voltage": "",
                 "motor": "",
@@ -63,7 +63,7 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER XL-25L",
             price: "₹2670",
             details: {
-                model:"XL-25L",
+                model: "XL-25L",
                 "power": "",
                 "rated_voltage": "",
                 "motor": "",
@@ -76,7 +76,7 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER XL-30L",
             price: "₹2560 ",
             details: {
-                model:" XTVC 42",
+                model: " XTVC 42",
                 "power": "",
                 "rated_voltage": "",
                 "motor": "",
@@ -89,7 +89,7 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER XL-38L ",
             price: "₹6270",
             details: {
-                "model":"XL-38L",
+                "model": "XL-38L",
                 "power": "1200W",
                 "rated_voltage": "230V",
                 "motor": "single",
@@ -102,7 +102,7 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER 6605-38A  ",
             price: "₹6135",
             details: {
-                "model":"XTVC 44",
+                "model": "XTVC 44",
                 "power": "1200W",
                 "rated_voltage": "230V",
                 "motor": "single",
@@ -115,7 +115,7 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER XL-60L-2M  ",
             price: "₹10935",
             details: {
-                "model":"XTVC 45",
+                "model": "XTVC 45",
                 "power": "1400W",
                 "rated_voltage": "230V",
                 "motor": "double",
@@ -128,7 +128,7 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER XL-60L-3M",
             price: "₹12535",
             details: {
-                "model":"XTVC 46",
+                "model": "XTVC 46",
                 "power": "2400W",
                 "rated_voltage": "230V",
                 "motor": "triple",
@@ -141,12 +141,76 @@ const VaccumCleaner= () => {
             title: "VACCUM CLEANER 606-80 (2M) ",
             price: "₹14722",
             details: {
-                "model":"XTVC 47",
+                "model": "XTVC 47",
                 "power": "2400W",
                 "rated_voltage": "230V",
                 "motor": "double",
                 "tank": "80L",
                 "wind_volume": "5.6m3/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/4NPPdFzw/image.png",
+            title: "VACCUM CLEANER XL-80L (3M)",
+            price: "₹11100",
+            details: {
+                "model": "XTVC 48",
+                "POWER": "3600W",
+                "RATED_VOLTAGE": "230V",
+                "MOTOR": "TRIPLE",
+                "TANK": "80L",
+                "WIND_VOLUME": "5.6M3/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/GmhC8N6p/image.png",
+            title: "CORDLESS VACCUM CLEANER",
+            price: "₹13500",
+            details: {
+                "Model": "XTVC 49",
+                "POWER": "2400W",
+                "RATED_VOLTAGE": "230V",
+                "MOTOR": "DOUBLE",
+                "WIND_VOLUME": "5.6M3/min"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/j5Jn7vLD/image.png",
+            title: "VACCUM CLEANER - GW-20L",
+            price: "₹3215",
+            details: {
+                "Model": "GW-20L",
+                "input_power": "1000W",
+                "rated_voltage": "230~",
+                "wind_volume": "2.4 m3/min",
+                "hz": "50",
+                "tank": "20L"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/V6xwJWCL/image.png",
+            title: "VACCUM CLEANER - GW-38L",
+            price: "₹5890",
+            details: {
+                "Model": "GW-38L",
+                "input_power": "1400W",
+                "rated_voltage": "230-240~",
+                "wind_volume": "2.6 m3/min",
+                "hz": "50-60",
+                "tank": "38L"
+            }
+        },
+        {
+            imageUrl: "https://i.postimg.cc/6qZMQdrK/image.png",
+            title: "VACUUM CLEANER - GW-60L-3M",
+            price: "₹5890",
+            details: {
+                "Model": "GW-60L-3M",
+                "input_power": "3*1400W",
+                "rated_voltage": "220-240",
+                "wind_volume": "2.6 x3m/min",
+                "hz": "50-60",
+                "tank": "60L"
             }
         },
     ];
