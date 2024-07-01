5ff3e7182928b26c1d56040a0987c796e87f5793
Pranav <responsetopranav@gmail.com>
2024-07-01 14:51:15 +0530

    Mig photos

--- src/components/Cards/MigSpareParts.jsx
+++ src/components/Cards/MigSpareParts.jsx
@@ -2,62 +2,92 @@ import React from 'react';
 import CardComponent from '../CardComponent';
 import { Box, Grid } from '@mui/material';
 
-const MigSpareParts= () => {
+const MigSpareParts = () => {
     const cardData = [
         {
-            imageUrl: "https://i.postimg.cc/76KtzbW7/image.png",
-            title: "AUXILIARY GUN",
-            price: "₹5,500.00 + 18% Tax",
-            details: {
-}
-        },
-     
-        {
-            imageUrl: "https://i.postimg.cc/1XCj6442/image.png",
-            title: "BODY STRAIGHTNER BIG",
-            price: "₹13,500.00 + 18% Tax",
-            details: {
-            
-            }
-        },
-        {
-            imageUrl: "https://i.postimg.cc/Px3sHd1X/image.png",
-            title: "CARBON ROD (Pack of 10)",
-            price: "₹1,100.00 + 18% Tax",
-            details: {
-            
-            }
-        },
-        {
-            imageUrl: "https://i.postimg.cc/7L1X0XLj/image.png",
-            title: "CARBON ROD HOLDER",
-            price: "₹350.00 + 18% Tax",
-            details: {
-            
-            }
-        },
-        {
-            imageUrl: "https://i.postimg.cc/mrDLvpHG/image.png",
-            title: "CLAW JAW HAMMER SET",
-            price: "₹4,500.00 + 18% Tax",
-            details: {
-            
-            }
-        },
-        {
-            imageUrl: "https://i.postimg.cc/gkmnMDNG/image.png",
-            title: "EARTH CLAMP",
-            price: "₹2,200.00 + 18% Tax",
+            imageUrl: "https://i.postimg.cc/FsJpgjk4/Screenshot-2024-07-01-144507.png",
+            title: "",
+            price: "",
             details: {}
         },
         {
-            imageUrl: "https://i.postimg.cc/MZR90Wrh/image.png",
-            title: "ELONG WASHER STRAIGHT (Pack of 50)",
-            price: "₹1,000.00 + 18% Tax",
-            details: {
-            
-            }
+            imageUrl: "https://i.postimg.cc/26Kdn6zy/Screenshot-2024-07-01-144520.png",
+            title: "",
+            price: "",
+            details: {}
         },
+        {
+            imageUrl: "https://i.postimg.cc/0QQd6m1h/Screenshot-2024-07-01-144532.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/xd9P2hxf/Screenshot-2024-07-01-144541.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/sgdcnv5Z/Screenshot-2024-07-01-144604.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/CxG49S4r/Screenshot-2024-07-01-144613.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/8CwmP49h/Screenshot-2024-07-01-144621.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/44YqqHH8/Screenshot-2024-07-01-144629.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/pr5fCHTZ/Screenshot-2024-07-01-144637.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/fk4xgrSh/Screenshot-2024-07-01-144644.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/DfRHTfq7/Screenshot-2024-07-01-144708.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/cJ15mvNB/Screenshot-2024-07-01-144719.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/GpMgSbLw/Screenshot-2024-07-01-144738.png",
+            title: "",
+            price: "",
+            details: {}
+        },
+        {
+            imageUrl: "https://i.postimg.cc/7ZkR5Bsc/Screenshot-2024-07-01-144747.png",
+            title: "",
+            price: "",
+            details: {}
+        }
     ];
 
     return (
